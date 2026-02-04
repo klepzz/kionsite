d3588838fcb2b55ffc90ed5f1179b722790b3cb1
Yusufhan Öztürk <93738594+klepzz@users.noreply.github.com>
2026-02-04 10:27:24 +0300

    Update page.tsx

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -188,7 +188,7 @@ export default async function BlogPostPage({
                 }}
             />
 
-            <main className="pb-24 pt-24 relative">
+            <main className="pb-24 pt-28 relative">
                 {/* Tables of Interactivity */}
                 <Reactions slug={post.slug} />
 
