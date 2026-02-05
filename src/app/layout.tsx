f98026922715f63fa094dc126f093ee904cf8e13
Kion Updates <updates@kionsite.com>
2026-02-05 14:44:16 +0300

    Fix: Add missing Script import causing build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 import { Playfair_Display, Lato } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./components/ThemeProvider";
